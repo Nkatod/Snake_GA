633a6863e964c5b900ce712cd1bf839369a7f521
Nkatod <51971642+Nkatod@users.noreply.github.com>
2020-10-20 14:45:18 +0530

    Update SnakeGA.py

--- SnakeGA.py
+++ SnakeGA.py
@@ -1,4 +1,4 @@
-import pygame, sys, random, time
+import pygame, random, time
 import gym
 from gym import spaces
 import numpy as np
@@ -11,7 +11,7 @@ import numpy as np
         - nsurv - количество выживших
         - reverse - указываем требуемую операцию поиска результата: максимизация или минимизация
 '''
-game_heigh = 15
+agent_size = 9
 def getSurvPopul(
         popul,
         val,
@@ -21,7 +21,7 @@ def getSurvPopul(
     newpopul = []  # Двумерный массив для новой популяции
 
     for i in range(len(val)):
-        if val[i] != 0:
+        if val[i] > 0:
             val[i] = val[i] + np.random.random() * 0.001
 
     sval = sorted(val, reverse=reverse)  # Сортируем зачения в val в зависимости от параметра reverse
@@ -32,7 +32,7 @@ def getSurvPopul(
         index = val.index(sval[i])  # Получаем индекс i-того элемента sval в исходном массиве val
         newpopul.append(popul[index])  # В новую папуляцию добавляем элемент из текущей популяции с найденным индексом
     while len(newpopul) < nsurv:
-        bot = np.random.random((game_heigh, 3))
+        bot = np.random.random((agent_size, 3))
         newpopul.append(bot)
 
     return newpopul, sval  # Возвращаем новую популяцию (из nsurv элементов) и сортированный список
@@ -92,9 +92,11 @@ class Snake(gym.Env):
         self.id = "Snake"
         self.reward_threshold = 0.0
         self.trials = 100
-        self.max_episode_steps = 100
+        self.max_episode_steps = 2000
         self.mode = "auto"
         self.current_step = 0
+        self.energy = 50
+        self.score = 0
 
         # make it square for the sake of defining the state space
         self.height = width
@@ -106,6 +108,9 @@ class Snake(gym.Env):
         self.thickness = thickness
         self.gameMatrix = np.zeros((self.width, self.width))
         self.agent = bot
+        self.pygame = pygame
+        self.pygame.init()
+        self.fpsController = self.pygame.time.Clock()
         # np.zeros((self.width, len(self.possible_actions)))
         # whether it should render anything
         # self.display = display
@@ -125,21 +130,170 @@ class Snake(gym.Env):
         self.pygame.quit()
 
     def get_states(self):
-        ''' Gets the relevant states of the game used in the AI
-        '''
-        state_dict = {
-            'snake_pos_x': self.snakePos[1],
-            'snake_pos_y': self.snakePos[0],
-            'food_pos_x': self.foodPos[1],
-            'food_pos_y': self.foodPos[0],
-            #             'snake_body': self.snakeBody,
-            # 'is_game_over': 1 if self.game_over else 0]],
-            'border_distances_right': abs((self.width - 1) - self.snakePos[0]),
-            'border_distances_left': self.snakePos[0],
-            'border_distances_up': abs((self.height - 1) - self.snakePos[1]),
-            'border_distances_down': self.snakePos[1]
-        }
+
+        isfood_forward = 0.0
+        isfood_left = 0.0
+        isfood_right = 0.0
+        cellfree_left = 1.0
+        cellfree_right = 1.0
+        cellfree_forward = 1.0
+        cell2free_left = 1.0
+        cell2free_right = 1.0
+        cell2free_forward = 1.0
+
+        if self.direction == 'RIGHT':
+            if self.snakePos[0] < self.foodPos[0]:
+                isfood_forward = 1.0
+            elif self.snakePos[0] > self.foodPos[0]:
+                isfood_right = 1.0
+                isfood_left = 1.0
+            if self.snakePos[1] < self.foodPos[1]:
+                isfood_right = 1.0
+            elif self.snakePos[1] > self.foodPos[1]:
+                isfood_left = 1.0
+            if self.snakePos[0] + 1 >= self.height:
+                cellfree_forward = 0.0
+            else:
+                cellfree_forward = self.gameMatrix[self.snakePos[0] + 1][self.snakePos[1]]
+            if self.snakePos[1] - 1 < 0:
+                cellfree_left = 0.0
+            else:
+                cellfree_left = self.gameMatrix[self.snakePos[0]][self.snakePos[1] - 1]
+            if self.snakePos[1] + 1 >= self.height:
+                cellfree_right = 0.0
+            else:
+                cellfree_right = self.gameMatrix[self.snakePos[0]][self.snakePos[1] + 1]
+            if self.snakePos[0]+2 >= self.height:
+                cell2free_forward = 0.0
+            else:
+                cell2free_forward = self.gameMatrix[self.snakePos[0]+2][self.snakePos[1]]
+            if self.snakePos[1] - 2 < 0:
+                cell2free_left = 0.0
+            else:
+                cell2free_left = self.gameMatrix[self.snakePos[0]][self.snakePos[1] - 2]
+            if self.snakePos[1] + 2 >= self.height:
+                cell2free_right = 0.0
+            else:
+                cell2free_right = self.gameMatrix[self.snakePos[0]][self.snakePos[1] + 2]
+
+        if self.direction == 'LEFT':
+            if self.snakePos[0] > self.foodPos[0]:
+                isfood_forward = 1.0
+            else:
+                isfood_right = 1.0
+                isfood_left  = 1.0
+            if self.snakePos[1] > self.foodPos[1]:
+                isfood_right = 1.0
+            elif self.snakePos[1] < self.foodPos[1]:
+                isfood_left = 1.0
+            if self.snakePos[0] - 1 < 0:
+                cellfree_forward = 0.0
+            else:
+                cellfree_forward = self.gameMatrix[self.snakePos[0]-1][self.snakePos[1]]
+            if self.snakePos[1] + 1 >= self.height:
+                cellfree_left = 0.0
+            else:
+                cellfree_left = self.gameMatrix[self.snakePos[0]][self.snakePos[1] + 1]
+            if self.snakePos[1] - 1 < 0:
+                cellfree_right = 0.0
+            else:
+                cellfree_right = self.gameMatrix[self.snakePos[0]][self.snakePos[1] - 1]
+            if self.snakePos[0] - 2 < 0:
+                cell2free_forward = 0.0
+            else:
+                cell2free_forward = self.gameMatrix[self.snakePos[0]-2][self.snakePos[1]]
+            if self.snakePos[1] + 2 >= self.height:
+                cell2free_left = 0.0
+            else:
+                cell2free_left = self.gameMatrix[self.snakePos[0]][self.snakePos[1] + 2]
+            if self.snakePos[1] - 2 < 0:
+                cell2free_right = 0.0
+            else:
+                cell2free_right = self.gameMatrix[self.snakePos[0]][self.snakePos[1] - 2]
+
+        if self.direction == 'UP':
+            if self.snakePos[1] > self.foodPos[1]:
+                isfood_forward = 1.0
+            else:
+                isfood_right = 1.0
+                isfood_left  = 1.0
+            if self.snakePos[0] < self.foodPos[0]:
+                isfood_right = 1.0
+            elif self.snakePos[0] > self.foodPos[0]:
+                isfood_left = 1.0
+            if self.snakePos[1] - 1 < 0:
+                cellfree_forward = 0.0
+            else:
+                cellfree_forward = self.gameMatrix[self.snakePos[0]][self.snakePos[1]-1]
+            if self.snakePos[0] - 1 < 0:
+                cellfree_left = 0.0
+            else:
+                cellfree_left = self.gameMatrix[self.snakePos[0] - 1][self.snakePos[1]]
+            if self.snakePos[0] + 1 >= self.height:
+                cellfree_right = 0.0
+            else:
+                cellfree_right = self.gameMatrix[self.snakePos[0] + 1][self.snakePos[1]]
+            if self.snakePos[1] - 2 < 0:
+                cell2free_forward = 0.0
+            else:
+                cell2free_forward = self.gameMatrix[self.snakePos[0]][self.snakePos[1]-2]
+            if self.snakePos[0] - 2 < 0:
+                cell2free_left = 0.0
+            else:
+                cell2free_left = self.gameMatrix[self.snakePos[0] - 2][self.snakePos[1]]
+            if self.snakePos[0] + 2 >= self.height:
+                cell2free_right = 0.0
+            else:
+                cell2free_right = self.gameMatrix[self.snakePos[0] + 2][self.snakePos[1]]
+
+        if self.direction == 'DOWN':
+            if self.snakePos[1] < self.foodPos[1]:
+                isfood_forward = 1.0
+            else:
+                isfood_right = 1.0
+                isfood_left  = 1.0
+            if self.snakePos[0] > self.foodPos[0]:
+                isfood_right = 1.0
+            elif self.snakePos[0] < self.foodPos[0]:
+                isfood_left = 1.0
+            if self.snakePos[1] + 1 >= self.height:
+                cellfree_forward = 0.0
+            else:
+                cellfree_forward = self.gameMatrix[self.snakePos[0]][self.snakePos[1]+1]
+            if self.snakePos[0] + 1 >= self.height:
+                cellfree_left = 0.0
+            else:
+                cellfree_left = self.gameMatrix[self.snakePos[0] + 1][self.snakePos[1]]
+            if self.snakePos[0] - 1 < 0:
+                cellfree_right = 0.0
+            else:
+                cellfree_right = self.gameMatrix[self.snakePos[0] - 1][self.snakePos[1]]
+            if self.snakePos[1] + 2 >= self.height:
+                cell2free_forward = 0.0
+            else:
+                cell2free_forward = self.gameMatrix[self.snakePos[0]][self.snakePos[1]+2]
+            if self.snakePos[0] + 2 >= self.height:
+                cell2free_left = 0.0
+            else:
+                cell2free_left = self.gameMatrix[self.snakePos[0] + 2][self.snakePos[1]]
+            if self.snakePos[0] - 2 < 0:
+                cell2free_right = 0.0
+            else:
+                cell2free_right = self.gameMatrix[self.snakePos[0] - 2][self.snakePos[1]]
+
+        state_dict = np.array([isfood_left,
+                               isfood_forward,
+                               isfood_right,
+                               cellfree_left,
+                               cellfree_right,
+                               cellfree_forward,
+                               cell2free_left,
+                               cell2free_right,
+                               cell2free_forward
+                                ])
+
         return state_dict
+
 
     def render_game_over(self):
         if self.game_over:
@@ -171,9 +325,9 @@ class Snake(gym.Env):
             if event.type == self.pygame.QUIT:
                 self.game_over = True
             elif event.type == self.pygame.KEYDOWN:
-                if event.key == self.pygame.K_RIGHT or event.key == ord('d'):
+                if event.key == self.pygame.K_right or event.key == ord('d'):
                     change_to = 'RIGHT'
-                if event.key == self.pygame.K_LEFT or event.key == ord('a'):
+                if event.key == self.pygame.K_left or event.key == ord('a'):
                     change_to = 'LEFT'
                 if event.key == self.pygame.K_ESCAPE:
                     self.pygame.event.post(self.pygame.event.Event(self.pygame.QUIT))
@@ -223,6 +377,7 @@ class Snake(gym.Env):
         self.snakeBody.insert(0, list(self.snakePos))
         if self.is_in_food_position():
             self.score += 1
+            self.energy += 25
             self.foodSpawn = False
             # test not growing snake
         #             self.snakeBody.pop()
@@ -245,21 +400,23 @@ class Snake(gym.Env):
         # Bound
         if self.snakePos[0] > (self.height - 1) or self.snakePos[0] < 0:
             self.game_over = True
+            self.score -= 5
         if self.snakePos[1] > (self.width - 1) or self.snakePos[1] < 0:
             self.game_over = True
+            self.score -= 5
         # Self hit
         for block in self.snakeBody[1:]:
             if self.snakePos[0] == block[0] and self.snakePos[1] == block[1]:
                 self.game_over = True
-        self.current_step += 1
-        if self.current_step > self.max_episode_steps:
+                self.score -= 5
+        self.energy -= 1
+        if self.current_step > self.max_episode_steps or self.energy <= 0:
             self.game_over = True
 
     def play_game(self):
         #self.init_interface()
         while self.game_over == False:
             #direction = self.get_pressed_key()
-            self.updateGameMatrix()
             direction = self.get_direction_from_GA()
             self.move_snake(direction)
             if self.mode != "auto":
@@ -276,8 +433,14 @@ class Snake(gym.Env):
         self.spawn_food()
         self.game_over = False
         self.direction = 'RIGHT'
+        self.energy = 50
         self.score = 0
         self.current_step = 0
+        self.setFPSTick(5000)
+
+    def setFPSTick(self, FPS):
+        self.fpsController = self.pygame.time.Clock()
+        self.fpsController.tick(FPS)
 
     def render(self, mode='human', close=False):
         ''' Render method for openai
@@ -303,13 +466,9 @@ class Snake(gym.Env):
                                                self.thickness, self.thickness))
         self.render_score()
         self.pygame.display.flip()
-        self.fpsController.tick(10000)
         # self.draw_toColab()
 
     def init_interface(self):
-        self.pygame = pygame
-        self.pygame.init()
-        self.fpsController = self.pygame.time.Clock()
         # Play surface
         self.playSurface = self.pygame.display.set_mode((self.height * self.thickness, self.width * self.thickness))
         self.pygame.display.set_caption('Snake game!')
@@ -317,18 +476,20 @@ class Snake(gym.Env):
     def updateGameMatrix(self):
         for i in range(self.gameMatrix.shape[0]):
             for j in range(self.gameMatrix.shape[1]):
-                self.gameMatrix[i][j] = 0.01
+                self.gameMatrix[i][j] = 0.75
         # update snake pos
         for i in self.snakeBody:
-            self.gameMatrix[i[0]][i[1]] = 0.5;
-        self.gameMatrix[self.snakePos[0]][self.snakePos[1]] = 0.4
-        self.gameMatrix[self.foodPos[0]][self.foodPos[1]] = 0.99
+            self.gameMatrix[i[0]][i[1]] = 0.40;
+        self.gameMatrix[self.snakePos[0]][self.snakePos[1]] = 0.00
+        self.gameMatrix[self.foodPos[0]][self.foodPos[1]] = 1.0
 
     def get_direction_from_GA(self):
-        newm = self.gameMatrix.flatten()
-        result = np.dot(self.gameMatrix, self.agent)
-        result = np.max(result, axis=0)
+        self.updateGameMatrix()
+        states = self.get_states()
+        # result = np.dot(self.gameMatrix, self.agent)
+        # result = np.sum(result, axis=0)
         # result = np.dot(result, np.ones((20,3)))
+        result = np.dot(states, self.agent)
         action = np.argmax(result)
         return self.possible_actions[action]
 
@@ -338,29 +499,33 @@ if __name__ == '__main__':
     popul = []  # здесь будет лежать популяция
     reward_list = []  # здесь будет сумма вознаграждений для каждого эпизода
     for i in range(numBots):
-        bot = np.random.random((game_heigh, 3))
+        bot = np.random.random((agent_size, 3))
         popul.append(bot)
     nsurv = 20  # количество выживших
     nnew = numBots - nsurv  # количество новых
     epohs = 500  # количество эпох
-    mut = 0.2  # коэфициент мутаций
+    mut = 0.01  # коэфициент мутаций
     mutStrength = 1e-1
     curr_time = time.time()
-    prevscore = 0
-    snake_game = Snake(width=game_heigh, thickness=20)
+    snake_game = Snake(width=20, thickness=20)
     snake_game.init_interface()
 
     for it in range(epohs):  # создали список списков всех значений по эпохам
         reward_list = []
+        showGame = False
         for bot in popul:
             snake_game.agent = bot
             snake_game.reset()
-            # if prevscore != currentscore:
-            #    snake_game.mode = 'render'
+            if showGame:
+                snake_game.mode = 'render'
+                snake_game.setFPSTick(FPS=1)
+                showGame = False
+            else:
+                snake_game.mode = 'auto'
             snake_game.play_game()
             reward_list.append(snake_game.score)
         newpopul, sval = getSurvPopul(popul = popul, val = reward_list, nsurv = nsurv, reverse=1)  # получили популяцию выживших, нас интересует бот с максимальным успехом, поэтому reverse = 1
-        print(it, time.time() - curr_time, " ",sval[0:3])  # Выводим время на операцию, среднее значение и 20 лучших ботов
+        print(it, time.time() - curr_time, " ", sval[0:3])  # Выводим время на операцию, среднее значение и 20 лучших ботов
         # проходимся по новой популяции
         for k in range(nnew):
             # вытаскиваем новых родителей
@@ -368,13 +533,13 @@ if __name__ == '__main__':
             newbot = []  # здесь будет новый бот
             for j in range(len(botp1)):  # боты-родители одинаковой длины, будем проходиться по каждому элементу родителя
                 x = crossPointFrom2Parents(botp1, botp2, j)  # скрещиваем
-                for t in range(botp1.shape[1]):
+                for t in range(x.shape[0]):
                     if random.random() < mut:
                         if random.random() < 0.5:
-                            x[t] = random.random()
+                            x[t] -= random.random() * mutStrength
                         else:
                             x[t] += random.random() * mutStrength
                 newbot.append(x)  # закидываем элемент в бота
             newpopul.append(newbot)  # добавляем бота
         popul = newpopul # вывести список на эпоху
-        popul = np.array(popul)  # для того, чтобы можно было легко вытащить индексы условием, преобразуем в numpy массив
+        #popul = np.array(popul)  # для того, чтобы можно было легко вытащить индексы условием, преобразуем в numpy массив
